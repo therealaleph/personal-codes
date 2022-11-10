8ccaa25fa984f166d908c095687b3fbd001650e0
Aleph (AKA no_itsmyturn) <67456590+therealaleph@users.noreply.github.com>
2022-11-10 18:47:22 +0000

    Update tj.sh

--- tj.sh
+++ tj.sh
@@ -11,3 +11,6 @@ wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/mack-
 ( echo 1 ; echo 1; echo $domain; echo 1; echo n; echo; echo; echo; ) | bash /root/install.sh
 ( echo 18; echo 1; echo 11;)  | vasma
 ( echo 5; echo 1;) | vasma
+ufw allow 22,8080,8880,443,80,991,54321,2052,2082,2086,2095,2096/tcp && ufw allow 22,8080,8880,443,80,991,54321,2052,2082,2086,2095,2096/udp && ufw deny out from any to 10.0.0.0/8 && ufw deny out from any to 172.16.0.0/12 && ufw deny out from any to 192.168.0.0/16 && ufw deny out from any to 100.64.0.0/10 &&  ufw deny out from any to 198.18.0.0/15 && ufw deny out from any to 169.254.0.0/16
+(echo y ) | ufw enable 
+( echo 6; echo 10; echo 1; echo https://www.cloudflare.com;) | vasma 
